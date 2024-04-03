2c0231f9b4d69adfb47fef81332e9f9f3b62efa1
kaduk7 <zulakmal24@gmail.com>
2024-04-03 13:49:57 +0700

    asdasdasd

--- app/api/transaksi/[id]/route.ts
+++ app/api/transaksi/[id]/route.ts
@@ -1,3 +1,4 @@
+import { DetailTransaksiTb } from './../../../../node_modules/.prisma/client/index.d';
 import { NextResponse } from "next/server"
 import { PrismaClient } from "@prisma/client"
 
@@ -6,7 +7,7 @@ const prisma = new PrismaClient()
 
 
 export const GET = async (request: Request, { params }: { params: { id: string } }) => {
-   
+
     const transaksi = await prisma.transaksiTB.findMany({
         where: {
             tanggal: {
@@ -15,12 +16,11 @@ export const GET = async (request: Request, { params }: { params: { id: string }
             },
         },
         include: {
-            DetailJualTb: {
+            detailTransaksiTb: {
                 include: {
                     BarangTb: true,
                 }
-            }
-
+            },
         }
     });
     return NextResponse.json(transaksi, { status: 200 })
